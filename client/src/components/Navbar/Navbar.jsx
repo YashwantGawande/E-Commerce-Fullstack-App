c931637a453b7094f006bc7bc4d56c7338b5675b
YashwantNG <gawandeyashwant7@gmail.com>
2023-02-26 17:01:41 +0530

    update

---
+++ client/src/components/Navbar/Navbar.jsx
@@ -0,0 +1,15 @@
+import React from "react";
+
+const Navbar = () => {
+  return (
+    <div className="navbar">
+      <div className="wrapper">
+        <div className="left">left</div>
+        <div className="center">center</div>
+        <div className="right">right</div>
+      </div>
+    </div>
+  );
+};
+
+export default Navbar;
