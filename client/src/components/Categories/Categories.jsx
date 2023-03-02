3c50c3ecca3e79842893bfa71e35da159184ffd8
YashwantNG <gawandeyashwant7@gmail.com>
2023-03-02 21:14:16 +0530

    added categories with styles

--- client/src/components/Categories/Categories.jsx
+++ client/src/components/Categories/Categories.jsx
@@ -1,15 +1,89 @@
 import React from "react";
 import "./Categories.scss";
+import { Link } from "react-router-dom";
+
 const Categories = () => {
   return (
     <div className="categories">
-      <div className="parent">
-        <div className="div div1">1 </div>
-        <div className="div div2">2 </div>
-        <div className="div div3"> 3</div>
-        <div className="div div4">4 </div>
-        <div className="div div5"> 5</div>
-        <div className="div div6"> 6</div>
+      <div className="col">
+        <div className="row">
+          <img
+            src="https://images.pexels.com/photos/818992/pexels-photo-818992.jpeg?auto=compress&cs=tinysrgb&w=1600"
+            alt=""
+          />
+          <button>
+            <Link className="link" to="/products/1">
+              Sale
+            </Link>
+          </button>
+        </div>
+        <div className="row">
+          <img
+            src="https://images.pexels.com/photos/2036646/pexels-photo-2036646.jpeg?auto=compress&cs=tinysrgb&w=1600"
+            alt=""
+          />
+          <button>
+            <Link to="/products/1" className="link">
+              Women
+            </Link>
+          </button>
+        </div>
+      </div>
+      <div className="col">
+        <div className="row">
+          {" "}
+          <img
+            src="https://images.pexels.com/photos/1813947/pexels-photo-1813947.jpeg?auto=compress&cs=tinysrgb&w=1600"
+            alt=""
+          />
+          <button>
+            <Link to="/products/1" className="link">
+              New Season
+            </Link>
+          </button>
+        </div>
+      </div>
+      <div className="col col-l">
+        <div className="row">
+          <div className="col">
+            <div className="row">
+              <img
+                src="https://images.pexels.com/photos/1192609/pexels-photo-1192609.jpeg?auto=compress&cs=tinysrgb&w=1600"
+                alt=""
+              />
+              <button>
+                <Link to="/products/1" className="link">
+                  Men
+                </Link>
+              </button>
+            </div>
+          </div>
+          <div className="col">
+            <div className="row">
+              {" "}
+              <img
+                src="https://images.pexels.com/photos/2703202/pexels-photo-2703202.jpeg?auto=compress&cs=tinysrgb&w=1600"
+                alt=""
+              />
+              <button>
+                <Link to="/products/1" className="link">
+                  Accessories
+                </Link>
+              </button>
+            </div>
+          </div>
+        </div>
+        <div className="row">
+          <img
+            src="https://images.pexels.com/photos/1159670/pexels-photo-1159670.jpeg?auto=compress&cs=tinysrgb&w=1600"
+            alt=""
+          />
+          <button>
+            <Link to="/products/1" className="link">
+              Shoes
+            </Link>
+          </button>
+        </div>
       </div>
     </div>
   );
