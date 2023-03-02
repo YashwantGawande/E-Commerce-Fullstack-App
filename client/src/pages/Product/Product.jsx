fd2aebd1290958353187f77bb50f7a73f7e026de
YashwantNG <gawandeyashwant7@gmail.com>
2023-03-03 00:17:29 +0530

    updated product componenet with styles

--- client/src/pages/Product/Product.jsx
+++ client/src/pages/Product/Product.jsx
@@ -1,9 +1,26 @@
-import React from 'react'
+import React, { useState } from "react";
+import "./Product.scss";
 
 const Product = () => {
+  const [selectedImg, setSelectedImg] = useState(0);
+  const images = [
+    "https://images.unsplash.com/photo-1588117260148-b47818741c74?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=987&q=80",
+    "https://images.unsplash.com/photo-1508216310976-c518daae0cdc?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=1003&q=80",
+  ];
   return (
-    <div>Product</div>
-  )
-}
+    <div className="product">
+      <div className="left">
+        <div className="images">
+          <img src={images[0]} alt="" onClick={(e) => setSelectedImg(0)} />
+          <img src={images[1]} alt="" onClick={(e) => setSelectedImg(1)} />
+        </div>
+        <div className="mainImg">
+          <img src={images[selectedImg]} alt="" />
+        </div>
+      </div>
+      <div className="right"></div>
+    </div>
+  );
+};
 
-export default Product
+export default Product;
